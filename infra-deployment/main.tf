91668340aaad9c98415df473cf41e0af3615cc37
david-hankinson <davidmhankinson@outlook.com>
2024-12-08 00:05:05 -0500

    feat/ecr repo and repo name

--- infra-deployment/main.tf
+++ infra-deployment/main.tf
@@ -1,8 +1,8 @@
 resource "aws_ecr_repository" "rails-bank-trx-reporting" {
   name = "rails-bank-trx-reporting"
-  repository_name = "rails-bank-trx-reporting"
+  # repository_name = "rails-bank-trx-reporting"
 }
 
 output "repository_url" {
-  value = aws_ecr_repository.hello_world_repo.repository_url
+  value = aws_ecr_repository.rails-bank-trx-reporting.repository_url
 }
