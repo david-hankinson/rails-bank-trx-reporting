50925d9e7e1d260b7a4d9589a1deb21332d59e31
david-hankinson <davidmhankinson@outlook.com>
2024-12-07 23:12:44 -0500

    feat/ecr

--- infra-deployment/ecr/main.tf
+++ infra-deployment/ecr/main.tf
@@ -1,5 +1,2 @@
-module "ecr" {
-  source  = "terraform-aws-modules/ecr/aws"
-  version = "2.3.0"
-}
+
 
---
+++ infra-deployment/main.tf
@@ -0,0 +1,7 @@
+resource "aws_ecr_repository" "hello_world_repo" {
+  name = "hello-world-repo"
+}
+
+output "repository_url" {
+  value = aws_ecr_repository.hello_world_repo.repository_url
+}
