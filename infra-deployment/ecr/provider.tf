53b982ce060dde2bba1927fc3e2cecb31c4f3f5f
david-hankinson <davidmhankinson@outlook.com>
2024-12-07 22:58:48 -0500

    feat/ecr

--- infra-deployment/ecr/provider.tf
+++ infra-deployment/ecr/provider.tf
@@ -2,8 +2,14 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.54.1"
     }
+    backend "s3" {
+      bucket         = "rails-bank-trx-reporting"
+       key            = "terraform.tfstate"
+       region         = local.region
+       encrypt        = true
+    }
+
   }
 }
 module "ecr" {
