43c4dce51f0c2bf6470f2a8857e071d53dc2e951
david-hankinson <davidmhankinson@outlook.com>
2024-12-07 23:57:04 -0500

    feat/provider details in the root directory

--- infra-deployment/providers.tf
+++ infra-deployment/providers.tf
@@ -2,7 +2,7 @@ terraform {
      backend "s3" {
       bucket         = "rails-bank-trx-reporting"
        key            = "terraform.tfstate"
-       region         = local.region
+       # region         = local.region
        encrypt        = true
     }
   required_providers {
