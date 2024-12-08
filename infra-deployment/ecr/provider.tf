e14fccf06da4a702e2ee7c299b740144b61c8065
david-hankinson <davidmhankinson@outlook.com>
2024-12-07 23:56:37 -0500

    feat/provider details in the root directory

--- infra-deployment/ecr/provider.tf
+++ infra-deployment/ecr/provider.tf
@@ -1,22 +1,21 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-    }
-    backend "s3" {
-      bucket         = "rails-bank-trx-reporting"
-       key            = "terraform.tfstate"
-       region         = local.region
-       encrypt        = true
-    }
-
-  }
-}
-module "ecr" {
-  source  = "terraform-aws-modules/ecr/aws"
-  version = "2.3.0"
-}
-
-provider "aws" {
-  region = "ca-central-1"
-}
+# terraform {
+#      backend "s3" {
+#       bucket         = "rails-bank-trx-reporting"
+#        key            = "terraform.tfstate"
+#        region         = local.region
+#        encrypt        = true
+#     }
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#     }
+#   }
+# }
+# module "ecr" {
+#   source  = "terraform-aws-modules/ecr/aws"
+#   version = "2.3.0"
+# }
+#
+# provider "aws" {
+#   region = "ca-central-1"
+# }
--- infra-deployment/providers.tf
+++ infra-deployment/providers.tf
@@ -1,16 +1,16 @@
 terraform {
+     backend "s3" {
+      bucket         = "rails-bank-trx-reporting"
+       key            = "terraform.tfstate"
+       region         = local.region
+       encrypt        = true
+    }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
     }
-    backend "s3" {
-      bucket         = "rails-bank-trx-reporting"
-       key            = "terraform.tfstate"
-       encrypt        = true
-    }
   }
 }
-
 module "ecr" {
   source  = "terraform-aws-modules/ecr/aws"
   version = "2.3.0"
