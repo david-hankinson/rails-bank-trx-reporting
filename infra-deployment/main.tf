3f683910b2aaa79dcb458a4a8c032cc3e7f27d65
david-hankinson <davidmhankinson@outlook.com>
2024-12-07 23:52:05 -0500

    feat/provider details in the root directory

--- infra-deployment/main.tf
+++ infra-deployment/main.tf
@@ -5,3 +5,26 @@ resource "aws_ecr_repository" "hello_world_repo" {
 output "repository_url" {
   value = aws_ecr_repository.hello_world_repo.repository_url
 }
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+    backend "s3" {
+      bucket         = "rails-bank-trx-reporting"
+       key            = "terraform.tfstate"
+       region         = local.region
+       encrypt        = true
+    }
+
+  }
+}
+module "ecr" {
+  source  = "terraform-aws-modules/ecr/aws"
+  version = "2.3.0"
+}
+
+provider "aws" {
+  region = "ca-central-1"
+}
