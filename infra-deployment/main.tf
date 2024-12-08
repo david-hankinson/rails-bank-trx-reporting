b29c425a221515e497c7b1d8fa4578925767f267
david-hankinson <davidmhankinson@outlook.com>
2024-12-08 00:12:41 -0500

    tf destroy

--- infra-deployment/main.tf
+++ infra-deployment/main.tf
@@ -1,7 +1,7 @@
-module "ecr" {
-  source = "terraform-aws-modules/ecr/aws"
-
-  repository_name = "rails-bank-trx-reporting"
+# module "ecr" {
+#   source = "terraform-aws-modules/ecr/aws"
+#
+#   repository_name = "rails-bank-trx-reporting"
 
   # repository_read_write_access_arns = ["arn:aws:iam::012345678901:role/terraform"]
   # repository_lifecycle_policy = jsonencode({
