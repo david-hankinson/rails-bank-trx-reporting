e78a7993a1f11b11d4122557d90a49799878d812
david-hankinson <davidmhankinson@outlook.com>
2024-12-24 19:10:43 -0500

    feat/networking deploy via modular and environmental terragrunt

--- infra-deploy/infra-modules/network/03-alb.tf
+++ infra-deploy/infra-modules/network/03-alb.tf
@@ -40,3 +40,25 @@ resource "aws_lb_listener" "http" {
     target_group_arn = aws_lb_target_group.this.arn
   }
 }
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+  tags = {
+    Name = "internet_gateway"
+  }
+}
+
+resource "aws_route_table" "this" {
+  vpc_id = aws_vpc.this.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+}
+
+resource "aws_route_table_association" "public_subnets" {
+  for_each = { for idx, subnet_id in aws_subnet.public[*].id : idx => subnet_id }
+
+  subnet_id      = each.value
+  route_table_id = aws_route_table.this.id
+}
